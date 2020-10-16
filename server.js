03943ac0f48d9b43eaedb8b90ebed7e807f5212e
Jennifer Henry <jenhenry1995@gmail.com>
2020-10-15 19:48:45 -0700

    ughhhh

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const{ v4: uuidv4 } = require('uuid');
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -35,23 +35,30 @@ app.post('/api/notes', function(req,res){
 
     notesData.push(newNote);
 
-    fs.writeFile('./db/db.json', JSON.stringify(notesData), function(err){
+    fs.writeFile('./db/db.json', JSON.stringify(notesData), (err) => {
+        if(err){
+        throw err;
+    }
         res.json('Success!');
     });
 });
 
 app.delete('/api/notes/:id', function(req,res){
-    const id = req.params.id;
+    const id = rparseInt(eq.params.id);
+    const jsonNotes = JSON.parse(fs.readFileSync("./db/db.json"))
 
-    // notesData.splice(notesData.findIndex(i => i.id === id),1);
     for (i=0; i< notesData.length; i++){
         if(notesData[i].id === id){
             notesData.splice(i,1);
         }
     }
-
+    fs.writeFile('./db/db.json', JSON.stringify(notesData), (err) => {
+        if(err){
+        throw err;
+    }
+        res.json('Success!');
+    });
 });
-
 
 //this uses the public file to allow the HTML to acces the js
 // DEFINITION OF STATIC To serve static files such as images, CSS files, and JavaScript files, use the express.static built-in middleware function in Express.
